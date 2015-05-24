923a4147b983c7fea3b892262ba0c0a5b5f7225e
Josh Everett <josh@bandpage.com>
2015-05-24 01:00:56 -0700

    Add bootstrap.

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
